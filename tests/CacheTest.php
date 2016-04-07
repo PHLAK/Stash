2e52f75d13a015c5d9c8e1a9e1377b6ebca6ae42
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2016-04-06 23:29:53 -0700

    Added CacheTest

---
+++ tests/CacheTest.php
@@ -0,0 +1,22 @@
+<?php
+
+class CacheTest extends PHPUnit_Framework_TestCase {
+
+    protected $apc;
+    protected $file;
+    protected $memcache;
+
+    public function setUp() {
+        $this->apc      = Stash\Cache::make('apc', ['prefix' => 'stash_test']);
+        $this->file     = Stash\Cache::make('file', ['dir' => __DIR__ . '/cache']);
+        $this->memcache = Stash\Cache::make('memcache', ['host' => 'localhost', 'port' => '11211']);
+    }
+
+    /** @test */
+    public function it_is_an_instance_of_the_correct_class() {
+        $this->assertInstanceOf('Stash\Apc', $this->apc);
+        $this->assertInstanceOf('Stash\File', $this->file);
+        $this->assertInstanceOf('Stash\Memcache', $this->memcache);
+    }
+
+}
