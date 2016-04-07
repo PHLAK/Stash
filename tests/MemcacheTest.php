2f2ed4e3e98f6e55b71bb17d0782c35fffc3db52
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2016-04-06 23:34:47 -0700

    Added test cases for rememberForever() method

--- tests/MemcacheTest.php
+++ tests/MemcacheTest.php
@@ -66,6 +66,30 @@ class MemcacheTest extends PHPUnit_Framework_TestCase {
     }
 
     /** @test */
+    public function it_remembers_an_already_cached_item_forever() {
+
+        $this->stash->put('remember-me-forever', "Don't override me bro!", 5);
+
+        $text = $this->stash->rememberForever('remember-me-forever', function() {
+            return "I don't override him.";
+        });
+
+        $this->assertEquals("Don't override me bro!", $text);
+
+    }
+
+    /** @test */
+    public function it_remembers_a_nonexistant_item_forever() {
+
+        $date = $this->stash->rememberForever('remember-forever', function() {
+            return 'November 5th';
+        });
+
+        $this->assertEquals('November 5th', $date);
+
+    }
+
+    /** @test */
     public function it_can_forget_an_item() {
         $this->stash->put('forgettable', 'asdf', 5);
         $this->assertTrue($this->stash->forget('forgettable'));
